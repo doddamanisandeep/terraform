d5f9bfc21cbe7314bc179b8ade2f67bb6d2cf7e3
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2023-11-14 00:20:37 +0530

    Delete variables.tf

--- variables.tf
+++
@@ -1,9 +0,0 @@
-variable "ami_value" {
-    description = "value of ami"
-    type = string
-}
-
-variable "instance_type_value" {
-    description = "Value for type of instance"
-    type = string
-}
