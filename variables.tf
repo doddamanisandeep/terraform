7899aa8e107da9585f2f7f39cba0d5040270faa2
doddamanisandeep <98304236+doddamanisandeep@users.noreply.github.com>
2023-11-13 22:33:00 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,9 @@
+variable "ami_value" {
+    description = "value of ami"
+    type = string
+}
+
+variable "instance_type_value" {
+    description = "Value for type of instance"
+    type = string
+}
